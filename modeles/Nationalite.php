696e713e1bfbb151b7911ffb245aeae8c251eecf
HIS <hisjosuapro@gmail.com>
2023-03-08 17:13:38 +0100

    TP4 Partie 3 : Mise en place des modèles - class Nationalité 9:30

---
+++ modeles/Nationalite.php
@@ -0,0 +1,154 @@
+<?php
+class Nationalite {
+
+        /**
+         * Numero du nationalité
+         *
+         * @var int
+         */
+    private $num;
+
+        /**
+         * Libelle du nationalité
+         *
+         * @var string
+         */
+    private $libelle;
+
+    /**
+     * Numero du continent (clé étrangère) relié à num de Continent
+     *
+     * @var int
+     */
+    private $numContinent;
+    
+    /**
+     * Get the value of Num
+     */ 
+    public function getNum()
+    {
+    return $this->num;
+    }
+
+    /**
+     * Lit le libelle
+     *
+     * @return string
+     */
+    public function getLibelle() : string
+    {
+    return $this->libelle;
+    }
+
+    /**
+     * Ecrit dans le libelle
+     *
+     * @param string $libelle
+     * @return self
+     */
+    public function setLibelle(string $libelle) : self
+    {
+    $this->libelle = $libelle;
+
+    return $this;
+    }
+
+    /**
+     * Renvoie l'objet continent associé
+     * 
+     * @return Continent
+     */ 
+    public function getNumContinent() : Continent
+    {
+        return Continent::findById($this->numContinent);
+    }
+
+    /**
+     * Ecrit le num continent
+     *
+     * @param Continent $continent
+     * @return  self
+     */ 
+    public function setNumContinent(Continent $continent) : self
+    {
+        $this->numContinent = $continent->getNum();
+
+        return $this;
+    }
+
+    /**
+     * Retourne l'ensemble des Nationalite
+     *
+     * @return Nationalite[] tableau d'objet nationalite
+     */
+    public static function findAll() :array
+    {
+        $req=MonPdo::getInstance()->prepare("select n.num, n.libelle as 'libNation', c.libelle as 'libContinent'  from nationalite n, continent c where n.numContinent=c.num");
+        $req->setFetchMode(PDO::FETCH_OBJ);
+        $req->execute();
+        $lesResultats=$req->fetchAll();
+        return $lesResultats;
+    }
+
+    /**
+     * Trouve une nationalite par son num
+     *
+     * @param integer $id numéro de la nationalite
+     * @return Nationalite objet nationalite trouvé
+     */
+    public static function findById(int $id) :Nationalite
+    {
+        $req=MonPdo::getInstance()->prepare("Select * from nationalite where num= :id");
+        $req->setFetchMode(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE,'Nationalite');
+        $req->bindParam(':id', $id);
+        $req->execute();
+        $leResultats=$req->fetch();
+        return $leResultats;
+    }
+
+    /**
+     * Permet d'ajouter une nationalite
+     *
+     * @param Nationalite $nationalite nationalite à ajouter
+     * @return integer resultat (1 si l'opération a réussi, 0 sinon)
+     */
+    public static function add(Nationalite $nationalite) :int
+    {
+        $req=MonPdo::getInstance()->prepare("insert into nationalite(libelle,numContinent) values(:libelle, :numContinent)");
+        $req->bindParam(':libelle', $nationalite->getLibelle());
+        $req->bindParam(':numContinent', $nationalite->numContinent);
+        $nb=$req->execute();
+        return $nb;
+    }
+
+    /**
+     * Permet de modifier un nationalite
+     *
+     * @param Nationalite $nationalite nationalite à modifier
+     * @return integer (1 si l'opération a réussi, 0 sinon)
+     */
+    public static function update(Nationalite $nationalite) :int
+    {
+        $req=MonPdo::getInstance()->prepare("update nationalite set libelle= :libelle where num= :num");
+        $req->bindParam(':id', $nationalite->getNum());
+        $req->bindParam(':libelle', $nationalite->getLibelle());
+        $nb=$req->execute();
+        return $nb;
+    }
+
+    /**
+     * Permet de supprimer une nationalite
+     *
+     * @param Nationalite $nationalite
+     * @return integer
+     */
+    public static function delete(Nationalite $nationalite) :int
+    {
+        $req=MonPdo::getInstance()->prepare("delete drom nationalite where num= :id");
+        $req->bindParam(':id', $nationalite->getNum());
+        $nb=$req->execute();
+        return $nb;
+    }
+
+}
+?>
